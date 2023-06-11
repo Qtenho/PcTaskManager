f80be8be75d7eaa93380f3d8aa28d033f61c8d40
Qtenho <edwincuteno12@gmail.com>
2023-06-11 16:02:29 -0500

    Update backend y fronted

---
+++ TaskManager/TaskManager.Fronted/Repositories/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using TaskManager.Shared.Models;
+
+namespace TaskManager.Fronted.Repository
+{
+    public interface IRepository
+    {
+        Task<Response<T>> GetAsync<T>(string url);
+    }
+}
---
+++ TaskManager/TaskManager.Fronted/Repositories/Repository.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using TaskManager.Fronted.Repository;
+using TaskManager.Shared.Models;
+
+namespace TaskManager.Fronted.Repositories
+{
+    public class Repository : IRepository
+    {
+        private readonly HttpClient _httpClient;
+
+        public Repository(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<Response<T>> GetAsync<T>(string url)
+        {
+            var responseHttp = await _httpClient.GetAsync(url);
+            if (!responseHttp.IsSuccessStatusCode)
+            {
+                return new Response<T>
+                {
+                    IsSuccess = false,
+                    Message = "Fail to get results"
+                };
+            }
+
+            var responseString = await responseHttp.Content.ReadAsStringAsync();
+            var responseJson = JsonSerializer.Deserialize<T>(responseString);
+            return new Response<T>
+            {
+                IsSuccess = true,
+                Result = responseJson
+            };
+
+        }
+    }
+}
